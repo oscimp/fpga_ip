ea8f3df22cc3aed07edd1b2582c2f8c7b491a80c
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2018-12-20 10:16:13 +0100

    shifterReal IP used to shift, to the right or the left, input data to match output data size. - if input data size == output data size: input is just copied to output; - if input data size < output data size: output data are shift left   (multiplication by 2^n) by adding output data size - input data size in LSB; - if input data size > output data size: output data are shift right   (divide by 2^n) by suppressing output data size - input data size in LSB;

---
+++ shifterReal/xgui/shifterReal_v1_0.tcl
@@ -0,0 +1,40 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "Component_Name" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DATA_IN_SIZE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DATA_OUT_SIZE" -parent ${Page_0}
+
+
+}
+
+proc update_PARAM_VALUE.DATA_OUT_SIZE { PARAM_VALUE.DATA_OUT_SIZE } {
+	# Procedure called to update DATA_OUT_SIZE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DATA_OUT_SIZE { PARAM_VALUE.DATA_OUT_SIZE } {
+	# Procedure called to validate DATA_OUT_SIZE
+	return true
+}
+
+proc update_PARAM_VALUE.DATA_IN_SIZE { PARAM_VALUE.DATA_IN_SIZE } {
+	# Procedure called to update DATA_IN_SIZE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DATA_IN_SIZE { PARAM_VALUE.DATA_IN_SIZE } {
+	# Procedure called to validate DATA_IN_SIZE
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.DATA_IN_SIZE { MODELPARAM_VALUE.DATA_IN_SIZE PARAM_VALUE.DATA_IN_SIZE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DATA_IN_SIZE}] ${MODELPARAM_VALUE.DATA_IN_SIZE}
+}
+
+proc update_MODELPARAM_VALUE.DATA_OUT_SIZE { MODELPARAM_VALUE.DATA_OUT_SIZE PARAM_VALUE.DATA_OUT_SIZE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DATA_OUT_SIZE}] ${MODELPARAM_VALUE.DATA_OUT_SIZE}
+}
+
