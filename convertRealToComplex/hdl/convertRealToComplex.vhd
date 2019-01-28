87a15c6d260ae9266df412e231ac7a40e6926bbe
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2019-01-28 15:34:19 +0100

    convertRealToComplex: rename input signal and interface (data1 -> dataI, data2 -> dataQ).

--- convertRealToComplex/hdl/convertRealToComplex.vhd
+++ convertRealToComplex/hdl/convertRealToComplex.vhd
@@ -13,16 +13,16 @@ Entity convertRealToComplex is
 	);
 	port (
 		-- input data
-		data1_i : in std_logic_vector(DATA_SIZE-1 downto 0);
-		data1_en_i: in std_logic;
-		data1_eof_i: in std_logic := '0';
-		data1_rst_i: in std_logic;
-		data1_clk_i: in std_logic;
-		data2_i : in std_logic_vector(DATA_SIZE-1 downto 0) := (others => '0');
-		data2_en_i: in std_logic := '1';
-		data2_rst_i: in std_logic := '1';
-		data2_eof_i: in std_logic := '1';
-		data2_clk_i: in std_logic := '1';
+		dataI_i : in std_logic_vector(DATA_SIZE-1 downto 0);
+		dataI_en_i: in std_logic;
+		dataI_eof_i: in std_logic := '0';
+		dataI_rst_i: in std_logic;
+		dataI_clk_i: in std_logic;
+		dataQ_i : in std_logic_vector(DATA_SIZE-1 downto 0) := (others => '0');
+		dataQ_en_i: in std_logic := '1';
+		dataQ_rst_i: in std_logic := '1';
+		dataQ_eof_i: in std_logic := '1';
+		dataQ_clk_i: in std_logic := '1';
 		-- for the next component
 		data_i_o  : out std_logic_vector(DATA_SIZE-1 downto 0);
 		data_q_o  : out std_logic_vector(DATA_SIZE-1 downto 0);
@@ -36,11 +36,11 @@ end entity;
 ---------------------------------------------------------------------------
 Architecture convertRealToComplex_1 of convertRealToComplex is
 begin
-	data_clk_o <= data1_clk_i and data2_clk_i;	
-	data_rst_o <= data1_rst_i and data2_rst_i;
-	data_i_o <= data1_i;
-	data_q_o <= data2_i;
-	data_eof_o <= data1_eof_i and data2_eof_i;
-	data_en_o <= data1_en_i and data2_en_i;
+	data_clk_o <= dataI_clk_i and dataQ_clk_i;
+	data_rst_o <= dataI_rst_i and dataQ_rst_i;
+	data_i_o <= dataI_i;
+	data_q_o <= dataQ_i;
+	data_eof_o <= dataI_eof_i and dataQ_eof_i;
+	data_en_o <= dataI_en_i and dataQ_en_i;
 end architecture convertRealToComplex_1;
 
