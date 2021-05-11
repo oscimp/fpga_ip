4e2ef7348af0f206977af79c67af50722a2a3222
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2021-05-11 14:46:23 +0200

    cordicAtan: fix vivado stuff

--- cordicAtan/xgui/cordicAtan_v1_0.tcl
+++ cordicAtan/xgui/cordicAtan_v1_0.tcl
@@ -3,24 +3,24 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  set DATA_SIZE [ipgui::add_param $IPINST -name "DATA_SIZE" -parent ${Page_0}]
-  set_property tooltip {Size of input data bus.} ${DATA_SIZE}
+  set DATA_IN_SIZE [ipgui::add_param $IPINST -name "DATA_IN_SIZE" -parent ${Page_0}]
+  set_property tooltip {Size of input data bus.} ${DATA_IN_SIZE}
   set NB_ITER [ipgui::add_param $IPINST -name "NB_ITER" -parent ${Page_0}]
   set_property tooltip {Number of successive approximation.} ${NB_ITER}
-  set OUTPUT_SIZE [ipgui::add_param $IPINST -name "OUTPUT_SIZE" -parent ${Page_0}]
-  set_property tooltip {Size of output data bus.} ${OUTPUT_SIZE}
+  set DATA_OUT_SIZE [ipgui::add_param $IPINST -name "DATA_OUT_SIZE" -parent ${Page_0}]
+  set_property tooltip {Size of output data bus.} ${DATA_OUT_SIZE}
   set PI_VALUE [ipgui::add_param $IPINST -name "PI_VALUE" -parent ${Page_0}]
   set_property tooltip {Pi x pow(2, NB_ITER-1).} ${PI_VALUE}
 
 
 }
 
-proc update_PARAM_VALUE.DATA_SIZE { PARAM_VALUE.DATA_SIZE } {
-	# Procedure called to update DATA_SIZE when any of the dependent parameters in the arguments change
+proc update_PARAM_VALUE.DATA_IN_SIZE { PARAM_VALUE.DATA_IN_SIZE } {
+	# Procedure called to update DATA_IN_SIZE when any of the dependent parameters in the arguments change
 }
 
-proc validate_PARAM_VALUE.DATA_SIZE { PARAM_VALUE.DATA_SIZE } {
-	# Procedure called to validate DATA_SIZE
+proc validate_PARAM_VALUE.DATA_IN_SIZE { PARAM_VALUE.DATA_IN_SIZE } {
+	# Procedure called to validate DATA_IN_SIZE
 	return true
 }
 
@@ -33,12 +33,12 @@ proc validate_PARAM_VALUE.NB_ITER { PARAM_VALUE.NB_ITER } {
 	return true
 }
 
-proc update_PARAM_VALUE.OUTPUT_SIZE { PARAM_VALUE.OUTPUT_SIZE } {
-	# Procedure called to update OUTPUT_SIZE when any of the dependent parameters in the arguments change
+proc update_PARAM_VALUE.DATA_OUT_SIZE { PARAM_VALUE.DATA_OUT_SIZE } {
+	# Procedure called to update DATA_OUT_SIZE when any of the dependent parameters in the arguments change
 }
 
-proc validate_PARAM_VALUE.OUTPUT_SIZE { PARAM_VALUE.OUTPUT_SIZE } {
-	# Procedure called to validate OUTPUT_SIZE
+proc validate_PARAM_VALUE.DATA_OUT_SIZE { PARAM_VALUE.DATA_OUT_SIZE } {
+	# Procedure called to validate DATA_OUT_SIZE
 	return true
 }
 
@@ -57,14 +57,14 @@ proc update_MODELPARAM_VALUE.NB_ITER { MODELPARAM_VALUE.NB_ITER PARAM_VALUE.NB_I
 	set_property value [get_property value ${PARAM_VALUE.NB_ITER}] ${MODELPARAM_VALUE.NB_ITER}
 }
 
-proc update_MODELPARAM_VALUE.DATA_SIZE { MODELPARAM_VALUE.DATA_SIZE PARAM_VALUE.DATA_SIZE } {
+proc update_MODELPARAM_VALUE.DATA_IN_SIZE { MODELPARAM_VALUE.DATA_IN_SIZE PARAM_VALUE.DATA_IN_SIZE } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.DATA_SIZE}] ${MODELPARAM_VALUE.DATA_SIZE}
+	set_property value [get_property value ${PARAM_VALUE.DATA_IN_SIZE}] ${MODELPARAM_VALUE.DATA_IN_SIZE}
 }
 
-proc update_MODELPARAM_VALUE.OUTPUT_SIZE { MODELPARAM_VALUE.OUTPUT_SIZE PARAM_VALUE.OUTPUT_SIZE } {
+proc update_MODELPARAM_VALUE.DATA_OUT_SIZE { MODELPARAM_VALUE.DATA_OUT_SIZE PARAM_VALUE.DATA_OUT_SIZE } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.OUTPUT_SIZE}] ${MODELPARAM_VALUE.OUTPUT_SIZE}
+	set_property value [get_property value ${PARAM_VALUE.DATA_OUT_SIZE}] ${MODELPARAM_VALUE.DATA_OUT_SIZE}
 }
 
 proc update_MODELPARAM_VALUE.PI_VALUE { MODELPARAM_VALUE.PI_VALUE PARAM_VALUE.PI_VALUE } {
