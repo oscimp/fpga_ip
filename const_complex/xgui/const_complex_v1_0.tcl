2923cf7b74e55dd30fc8a5171525eda532f0f562
Ivan Ryger <om1air@gmail.com>
2021-11-09 12:58:45 +0100

    initial commit of const_real and const_complex, IR 9. Nov. 2021

---
+++ const_complex/xgui/const_complex_v1_0.tcl
@@ -0,0 +1,55 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "DATA_SIZE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "INT_IMAG_PART_OUT_VALUE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "INT_REAL_PART_OUT_VALUE" -parent ${Page_0}
+
+
+}
+
+proc update_PARAM_VALUE.DATA_SIZE { PARAM_VALUE.DATA_SIZE } {
+	# Procedure called to update DATA_SIZE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DATA_SIZE { PARAM_VALUE.DATA_SIZE } {
+	# Procedure called to validate DATA_SIZE
+	return true
+}
+
+proc update_PARAM_VALUE.INT_IMAG_PART_OUT_VALUE { PARAM_VALUE.INT_IMAG_PART_OUT_VALUE } {
+	# Procedure called to update INT_IMAG_PART_OUT_VALUE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.INT_IMAG_PART_OUT_VALUE { PARAM_VALUE.INT_IMAG_PART_OUT_VALUE } {
+	# Procedure called to validate INT_IMAG_PART_OUT_VALUE
+	return true
+}
+
+proc update_PARAM_VALUE.INT_REAL_PART_OUT_VALUE { PARAM_VALUE.INT_REAL_PART_OUT_VALUE } {
+	# Procedure called to update INT_REAL_PART_OUT_VALUE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.INT_REAL_PART_OUT_VALUE { PARAM_VALUE.INT_REAL_PART_OUT_VALUE } {
+	# Procedure called to validate INT_REAL_PART_OUT_VALUE
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.DATA_SIZE { MODELPARAM_VALUE.DATA_SIZE PARAM_VALUE.DATA_SIZE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DATA_SIZE}] ${MODELPARAM_VALUE.DATA_SIZE}
+}
+
+proc update_MODELPARAM_VALUE.INT_REAL_PART_OUT_VALUE { MODELPARAM_VALUE.INT_REAL_PART_OUT_VALUE PARAM_VALUE.INT_REAL_PART_OUT_VALUE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.INT_REAL_PART_OUT_VALUE}] ${MODELPARAM_VALUE.INT_REAL_PART_OUT_VALUE}
+}
+
+proc update_MODELPARAM_VALUE.INT_IMAG_PART_OUT_VALUE { MODELPARAM_VALUE.INT_IMAG_PART_OUT_VALUE PARAM_VALUE.INT_IMAG_PART_OUT_VALUE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.INT_IMAG_PART_OUT_VALUE}] ${MODELPARAM_VALUE.INT_IMAG_PART_OUT_VALUE}
+}
+
---
+++ const_real/hdl/const_real.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity const_real is 
+	generic(
+		DATA_SIZE : natural := 16;
+		INT_REAL_PART_OUT_VALUE : integer := 0
+		);
+	port(
+		--Syscon signals
+		ref_clk_i : in std_logic;
+		ref_rst_i : in std_logic;
+		data_i_o  : out std_logic_vector(DATA_SIZE - 1 downto 0);
+		data_en_o : out std_logic;
+		data_clk_o : out std_logic;
+		data_rst_o : out std_logic
+	);
+end entity const_real;
+architecture bhv of const_real is
+begin
+	data_i_o <= std_logic_vector(to_signed(INT_REAL_PART_OUT_VALUE,data_i_o'length));
+	data_en_o <= '1';
+	data_clk_o <= ref_clk_i;
+	data_rst_o <= ref_rst_i;
+end architecture bhv;
