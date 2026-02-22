38964861322a2d8e3293b4d577aa3e39c4e801a6
bma <bamarechal@free.fr>
2026-02-22 16:01:15 +0100

    multiplierReal: add ENABLE_SEL parameter     allow to select the rate of the output data

--- multiplierReal/hdl/multiplierReal.vhd
+++ multiplierReal/hdl/multiplierReal.vhd
@@ -9,6 +9,7 @@ use IEEE.numeric_std.ALL;
 entity multiplierReal is
 	generic (
 		SIGNED_FORMAT : boolean := true;
+		ENABLE_SEL : string := "and";
 		DATA1_IN_SIZE : natural := 16;
 		DATA2_IN_SIZE : natural := 16;
 		DATA_OUT_SIZE : natural := 2*16
@@ -50,12 +51,15 @@ architecture Behavioral of multiplierReal is
 
 	-- mult
 	signal data_s     : std_logic_vector(INT_DATA_SZ-1 downto 0);
+	signal data_clk_s  : std_logic;
+	signal data_rst_s  : std_logic;
 	signal data_en_s  : std_logic;
 	signal data_eof_s : std_logic;
 	signal data_sof_s : std_logic;
 begin
-	data_clk_o <= data1_clk_i;
-	data_rst_o <= data1_rst_i;
+
+	data_clk_o <= data_clk_s;
+	data_rst_o <= data_rst_s;
 
 	process(data1_clk_i)
 	begin
@@ -93,13 +97,35 @@ begin
 	unsigned_mult: if SIGNED_FORMAT = false generate
 		data_s <= std_logic_vector(unsigned(data1_in_s) * unsigned(data2_in_s));
 	end generate unsigned_mult;
-	data_en_s <= data1_in_en_s and data2_in_en_s;
-	data_eof_s <= data1_in_eof_s and data2_in_eof_s;
+
+	-- data1_in and data2_in for CANDR and en
+	and_sel: if ENABLE_SEL = "and" generate
+		data_clk_s <= data1_clk_i;
+		data_rst_s <= data1_rst_i;
+		data_en_s <= data1_in_en_s and data2_in_en_s;
+		data_eof_s <= data1_in_eof_s and data2_in_eof_s;
+	end generate and_sel;
+
+	-- data1_in for CANDR and en
+	data1_sel: if ENABLE_SEL = "data1" generate
+		data_clk_s <= data1_clk_i;
+		data_rst_s <= data1_rst_i;
+		data_en_s <= data1_in_en_s;
+		data_eof_s <= data1_in_eof_s;
+	end generate data1_sel;
+
+	-- data2_in for CANDR and en
+	data2_sel: if ENABLE_SEL = "data2" generate
+		data_clk_s <= data2_clk_i;
+		data_rst_s <= data2_rst_i;
+		data_en_s <= data2_in_en_s;
+		data_eof_s <= data2_in_eof_s;
+	end generate data2_sel;
 
 	redim_inst: entity work.multiplierReal_redim
 	generic map (SIGNED_FORMAT => SIGNED_FORMAT, IN_SZ => INT_DATA_SZ,
 		OUT_SZ => DATA_OUT_SIZE)
-	port map (clk_i => data1_clk_i, rst_i => data1_rst_i,
+	port map (clk_i => data_clk_s, rst_i => data_rst_s,
 		data_en_i => data_en_s, data_i => data_s,
 		data_eof_i => data_eof_s, data_sof_i => data_sof_s,
 		data_en_o => data_en_o, data_o => data_o,
--- multiplierReal/xgui/multiplierReal_v1_0.tcl
+++ multiplierReal/xgui/multiplierReal_v1_0.tcl
@@ -3,14 +3,11 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  set DATA1_IN_SIZE [ipgui::add_param $IPINST -name "DATA1_IN_SIZE" -parent ${Page_0}]
-  set_property tooltip {Size of the input data1 stream.} ${DATA1_IN_SIZE}
-  set DATA2_IN_SIZE [ipgui::add_param $IPINST -name "DATA2_IN_SIZE" -parent ${Page_0}]
-  set_property tooltip {Size of the input data2 stream.} ${DATA2_IN_SIZE}
-  set DATA_OUT_SIZE [ipgui::add_param $IPINST -name "DATA_OUT_SIZE" -parent ${Page_0}]
-  set_property tooltip {Size of the output data stream.} ${DATA_OUT_SIZE}
-  set SIGNED_FORMAT [ipgui::add_param $IPINST -name "SIGNED_FORMAT" -parent ${Page_0}]
-  set_property tooltip {signedOrunsigned.} ${SIGNED_FORMAT}
+  ipgui::add_param $IPINST -name "DATA1_IN_SIZE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DATA2_IN_SIZE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DATA_OUT_SIZE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "SIGNED_FORMAT" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "ENABLE_SEL" -parent ${Page_0} -widget comboBox
 
 
 }
@@ -42,6 +39,15 @@ proc validate_PARAM_VALUE.DATA_OUT_SIZE { PARAM_VALUE.DATA_OUT_SIZE } {
 	return true
 }
 
+proc update_PARAM_VALUE.ENABLE_SEL { PARAM_VALUE.ENABLE_SEL } {
+	# Procedure called to update ENABLE_SEL when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ENABLE_SEL { PARAM_VALUE.ENABLE_SEL } {
+	# Procedure called to validate ENABLE_SEL
+	return true
+}
+
 proc update_PARAM_VALUE.SIGNED_FORMAT { PARAM_VALUE.SIGNED_FORMAT } {
 	# Procedure called to update SIGNED_FORMAT when any of the dependent parameters in the arguments change
 }
@@ -70,5 +76,10 @@ proc update_MODELPARAM_VALUE.DATA2_IN_SIZE { MODELPARAM_VALUE.DATA2_IN_SIZE PARA
 proc update_MODELPARAM_VALUE.DATA_OUT_SIZE { MODELPARAM_VALUE.DATA_OUT_SIZE PARAM_VALUE.DATA_OUT_SIZE } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.DATA_OUT_SIZE}] ${MODELPARAM_VALUE.DATA_OUT_SIZE}
+}
+
+proc update_MODELPARAM_VALUE.ENABLE_SEL { MODELPARAM_VALUE.ENABLE_SEL PARAM_VALUE.ENABLE_SEL } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.ENABLE_SEL}] ${MODELPARAM_VALUE.ENABLE_SEL}
 }
 
